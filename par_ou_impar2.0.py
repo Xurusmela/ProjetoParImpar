398703ef4af52b089970b7817ce5f5804a66da48
BiaMayumi <109619902+BiaMayumi@users.noreply.github.com>
2023-10-16 14:50:13 -0300

    Commit inicial do tio.

---
+++ par_ou_impar2.0.py
@@ -0,0 +1,13 @@
+# Projeto par ou ímpar
+
+# Apresentação
+print('\n\t\t\t -- Verificador de Número -- \n\n')
+
+# Entrada
+num = int(input('Informe um número: '))
+
+# Processamento e Saída
+if num % 2 == 0:
+    print(f'\n{num} é par ')
+else:
+    print(f'\n{num} é Ímpar')
